187425706d20941208c8da27f4a628cf601bc551
tawing dela rama <yabuking84@gmail.com>
2022-02-10 17:00:21 +0400

    .

--- dist/request.js
+++ dist/request.js
@@ -1,4 +1,52 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const url = process.argv[2].split("=")[0];
-exports.default = url;
+const url_1 = __importDefault(require("./url"));
+const axios_1 = __importDefault(require("axios"));
+function startInterceptor(config) {
+    // console.log("startInterceptor config = ",config);
+    config.metadata = { startTime: new Date() };
+    return config;
+}
+;
+function endInterceptor(response) {
+    // console.log("endInterceptor response = ",response);
+    response.config.metadata.endTime = new Date();
+    response.duration = response.config.metadata.endTime - response.config.metadata.startTime;
+    return response;
+}
+;
+function isError(error) {
+    throw error;
+}
+function default_1() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            axios_1.default.interceptors.request.use(startInterceptor, isError);
+            axios_1.default.interceptors.response.use(endInterceptor, isError);
+            const res = yield axios_1.default.get(url_1.default);
+            console.log(`URL: ${url_1.default}`);
+            console.log(`Status: ${res.status}`);
+            // console.log(`Time Start: ${res.config.metadata.startTime}`);
+            // console.log(`Time End: ${res.config.metadata.endTime}`);
+            console.log(`Duration: ${res.duration}ms`);
+            console.log();
+        }
+        catch (error) {
+            console.error("ERROR: " + error.message);
+            console.log();
+        }
+    });
+}
+exports.default = default_1;
---
+++ src/request.ts
@@ -0,0 +1,49 @@
+import url from "./url"
+import axios, { AxiosRequestConfig } from "axios"
+import minimist from "minimist"
+
+
+function startInterceptor (config: any) {
+  // console.log("startInterceptor config = ",config);
+  config.metadata = { startTime: new Date() }
+  return config;
+};
+
+function endInterceptor (response: any) {
+  // console.log("endInterceptor response = ",response);
+  response.config.metadata.endTime = new Date()
+  response.duration = response.config.metadata.endTime - response.config.metadata.startTime
+  return response;
+};
+
+function isError(error: any){
+  throw error;
+}
+
+interface Res {
+  status: number,
+  duration: any,
+  config: {
+    metadata : {
+      startTime: any,
+      endTime: any
+    }
+  }
+}
+
+export default async function() {
+  try { 
+    axios.interceptors.request.use(startInterceptor,isError)
+    axios.interceptors.response.use(endInterceptor,isError)
+    const res = await axios.get(url) as Res
+    console.log(`URL: ${url}`);
+    console.log(`Status: ${res.status}`);
+    // console.log(`Time Start: ${res.config.metadata.startTime}`);
+    // console.log(`Time End: ${res.config.metadata.endTime}`);
+    console.log(`Duration: ${res.duration }ms`);
+    console.log();
+  } catch (error: any) {
+    console.error("ERROR: "+error.message);
+    console.log();
+  }
+}
